2582b120804b2f3efadd655932ad21d2524da511
Matías Bustos <agustinbustos_com@hotmail.com>
2021-06-12 02:59:32 -0400

    Adds dark theme

---
+++ frontend/src/themes/darkTheme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export default createMuiTheme({
+  typography: {
+    fontFamily: ["Poppins", "Arial", "sans-serif"],
+  },
+  palette: {
+    primary: {
+      main: "#311b92",
+    },
+    secondary: {
+      main: "#9c27b0",
+    },
+  },
+});
