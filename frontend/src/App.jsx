013bf9f1747c49d7829e87c08f772218538d06fc
Matías Bustos <agustinbustos_com@hotmail.com>
2021-06-12 03:00:10 -0400

    Adds theme provider to the App main component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import darkTheme from "./themes/darkTheme";
+
+const defaultTheme = darkTheme;
+
 function App() {
   return (
-    <div className="App">
-      Kiwi challenge
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Typography>Kiwi challenge</Typography>
+    </ThemeProvider>
   );
 }
 
