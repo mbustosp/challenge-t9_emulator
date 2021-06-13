856ee8a15ccb95c5b3e94455c736adf545f7a9c1
Matías Bustos <agustinbustos_com@hotmail.com>
2021-06-13 02:31:16 -0400

    Adds key component

---
+++ frontend/src/components/atoms/Key/Key.test.jsx
@@ -0,0 +1,49 @@
+import { ThemeProvider } from "@material-ui/core/styles";
+import { screen, render, fireEvent } from "@testing-library/react";
+import Key from "./index";
+import darkTheme from "../../../themes/darkTheme";
+
+describe("Key", () => {
+  it("renders without crash", () => {
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <Key />
+      </ThemeProvider>
+    );
+  });
+
+  it("renders primary and secondary symbols", () => {
+    const keyPrimarySymbol = 1;
+    const keySecondarySymbols = ["😀", "🙏", "👀"];
+    const onClick = jest.fn();
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <Key
+          onClick={onClick}
+          primarySymbol={keyPrimarySymbol}
+          secondarySymbols={keySecondarySymbols}
+        />
+      </ThemeProvider>
+    );
+    const foundKeyByNumber = screen.getByText(keyPrimarySymbol);
+    const foundKeysBySymbols = keySecondarySymbols.map((keySymbol) =>
+      screen.getByText(keySymbol)
+    );
+    expect(foundKeyByNumber).toBeTruthy();
+    foundKeysBySymbols.forEach((foundKeyBySymbol) =>
+      expect(foundKeyBySymbol).toBeTruthy()
+    );
+  });
+
+  it("calls onClick callback function when component is clicked", () => {
+    const keyPrimarySymbol = 1;
+    const onClick = jest.fn();
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <Key onClick={onClick} number={keyPrimarySymbol} />
+      </ThemeProvider>
+    );
+    fireEvent.click(screen.getByText(keyPrimarySymbol));
+    expect(onClick).toHaveBeenCalled();
+  });
+});
---
+++ frontend/src/components/atoms/Key/index.jsx
@@ -0,0 +1,103 @@
+/**
+ * Core dependencies
+ */
+import PropTypes from "prop-types";
+
+/**
+ * UI components
+ */
+import {
+  Box,
+  Button,
+  Typography,
+  Paper,
+  Grid,
+  createStyles,
+  makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    Paper: {
+      backgroundColor: "rgba(255, 255, 255, 0.25)",
+      boxShadow: "rgba(142, 142, 142, 0.19) 0px 6px 15px 0px",
+      color: "rgba(255, 255, 255, 0.75)",
+      borderRadius: "16px",
+      overflow: "hidden",
+    },
+    Button: {
+      padding: 0,
+    },
+    Symbol: {
+      padding: theme.spacing(0.3),
+    },
+  })
+);
+
+/**
+ * Button that represent a phone key button
+ */
+const Key = ({ primarySymbol, secondarySymbols, onClick }) => {
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={3} className={classes.Paper}>
+      <Button
+        className={classes.Button}
+        color="primary"
+        onClick={onClick}
+        variant="contained"
+        fullWidth
+      >
+        <Box display="flex" alignItems="center" p={2} height="60px">
+          <Grid container alignItems="center" justify="center">
+            {primarySymbol !== null ? (
+              <Grid item xs={12} container justify="center" alignItems="center">
+                {Number.isInteger(primarySymbol) ? (
+                  <Typography variant="h3">{primarySymbol}</Typography>
+                ) : (
+                  primarySymbol
+                )}
+              </Grid>
+            ) : null}
+
+            <Grid item xs={12}>
+              {secondarySymbols.map((secondarySymbol) => (
+                <Typography
+                  key={secondarySymbol}
+                  className={classes.Symbol}
+                  variant="button"
+                >
+                  {secondarySymbol}
+                </Typography>
+              ))}
+            </Grid>
+          </Grid>
+        </Box>
+      </Button>
+    </Paper>
+  );
+};
+
+Key.propTypes = {
+  /**
+   The main element of the key
+   */
+  primarySymbol: PropTypes.oneOfType([PropTypes.number, PropTypes.node]),
+  /**
+   Collection of elements that will be located under the main one
+   */
+  secondarySymbols: PropTypes.arrayOf(PropTypes.string),
+  /**
+   Callback function that will be called when the key button is pressed
+   */
+  onClick: PropTypes.func,
+};
+
+Key.defaultProps = {
+  primarySymbol: null,
+  secondarySymbols: [],
+  onClick: (e) => e,
+};
+
+export default Key;
