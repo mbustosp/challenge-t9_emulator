4174d2aab498fbeb996336651653e5ed6f3596a2
Matías Bustos <agustinbustos_com@hotmail.com>
2021-06-17 04:46:43 -0400

    Updates tests for suggestions endpoint

--- backend/routes/routes.test.js
+++ backend/routes/routes.test.js
@@ -4,9 +4,9 @@ const app = require("../app");
 describe("GET endpoints", () => {
   describe("/", () => {
     it("should retrieve the T9 prediction words", async () => {
-      const res = await request(app).get("/").query({ input: "56" });
+      const res = await request(app).get("/").query({ input: "2" });
       expect(res.statusCode).toEqual(200);
-      expect(res.body).toStrictEqual(["mock1", "mock2"]);
+      expect(res.body).toStrictEqual(["a", "c", "b"]);
     });
   });
 });
