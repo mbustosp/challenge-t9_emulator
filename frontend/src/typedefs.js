22deaa4fd9db5cab66a5424e58dabb84e7d1edf3
Matías Bustos <agustinbustos_com@hotmail.com>
2021-06-16 20:57:01 -0400

    Adds type definition file

---
+++ frontend/src/typedefs.js
@@ -0,0 +1,6 @@
+/**
+ @typedef T9Key
+ @property {string} value A string representation of the t9 keyboard's digit
+ @property {string} symbol A string representation of the symbols this digit
+ represents
+ */
