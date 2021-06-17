ac662cff20abeda174375c7a62a370e36f436c80
Matías Bustos <agustinbustos_com@hotmail.com>
2021-06-16 22:33:05 -0400

    Fixes linting errors on files

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
+const router = express.Router();
+
+/* GET */
+router.get("/", (req, res) => {
   res.send(["mock1", "mock2"]);
 });
 
